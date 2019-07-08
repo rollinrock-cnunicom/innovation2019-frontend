3a305b801d1760f8a9cdbda4c7e02e5a271804b2
rollinrock <caojing9111@outlook.com>
2019-07-08 14:00:14 +0800

    选址初步功能实现

---
+++ src/views/station/evaluation.vue
@@ -0,0 +1,13 @@
+<template>
+  <map-locator></map-locator>
+</template>
+
+<script>
+import MapLocator from '../modules/MapLocater'
+export default {
+  components: {
+    MapLocator
+  }
+}
+</script>
+
